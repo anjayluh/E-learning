8aafb1ef015c123a553ca37b10b39700a692fd61
anjayluh <anjayluhgags.wakabi@gmail.com>
2016-11-16 18:22:42 +0300

    Second commit. It implements a home page and a partially commplete sign up page

--- elearning/urls.py
+++ elearning/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from . import views
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^users/', include('users.urls')),
+    url(r'^$', views.index, name = 'index'),
 ]
